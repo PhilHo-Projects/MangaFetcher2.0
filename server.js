24d6653da8d48181b1765ea39147b216fd028648
Phil <113161530+PhilippeHo27@users.noreply.github.com>
2025-10-09 20:24:00 -0400

    port 3001

--- server.js
+++ server.js
@@ -147,8 +147,8 @@ app.get('/api/next-check', (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(3001, () => {
+  console.log('Server running on port 3001');
   
   // Start the scheduler
   scheduleChapterCheck();
